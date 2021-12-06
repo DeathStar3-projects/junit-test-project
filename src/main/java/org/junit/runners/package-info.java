fc5898cf163674c6ef381f5d018fd96e2d516cc4
ANAGONOU Patrick <anagonousourou@gmail.com>
2021-12-06 17:39:37 +0100

    first commit

---
+++ src/main/java/org/junit/runners/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides standard {@link org.junit.runner.Runner Runner} implementations.
+ *
+ * @see org.junit.runner.Runner
+ * @see org.junit.runners.BlockJUnit4ClassRunner
+ * @since 4.0
+ */
+package org.junit.runners;
